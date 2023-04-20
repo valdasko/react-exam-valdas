dd8af268659d5ebbc0d483a7062174348b2658c9
Valdas <valdaskontenis1@gmail.com>
2023-04-20 15:15:08 +0300

    deleted strict

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,11 @@ import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './store/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
