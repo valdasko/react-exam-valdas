45bce192d70b3cf8161c40319b651731e1b944ae
Valdas <valdaskontenis1@gmail.com>
2023-04-21 11:04:56 +0300

    custom css colors and fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return <div className='text-headers'>Home</div>;
 }
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#CDA274',
+        secondary: '#292F36',
+        light: '#F4F0EC',
+      },
+      fontFamily: {
+        body: ['Jost', 'sans-serif'],
+        headers: ['DM Serif Display', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
