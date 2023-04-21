baf7f06a72f8db4f20c075f49589e095181b1ea1
Valdas <valdaskontenis1@gmail.com>
2023-04-21 15:50:20 +0300

    header changes color after scroll down with custom hook

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,39 +1,53 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { useAuthCtx } from '../../store/AuthProvider';
+import { useScrollPosition } from '../../hooks/useScrollPosition';
 
 function Header() {
   const { isLoggedIn, logout } = useAuthCtx();
+
+  const scrollPosition = useScrollPosition();
+  console.log('scrollPosition ===', scrollPosition);
+  function classNames(...clases) {
+    return clases.filter(Boolean).join(' ');
+  }
+
+  // className='sticky top-0 z-50  shadow-sm bg-white'
   return (
-    <header className='py-2'>
-      <div className='  max-w-[1200px]  mx-auto w-full  bg-light flex  justify-between items-center'>
-        <Link className='font-headers text-[40px]' to={'/'}>
+    <header
+      className={classNames(
+        scrollPosition > 0 ? 'shadow bg-white text-[#333]' : 'shadow:none',
+        'transition ease-out sticky top-0 z-50 bg-light text-secondary '
+      )}
+    >
+      <div className='max-w-[1200px]  mx-auto w-full flex  justify-between items-center '>
+        <Link className='font-headers text-[40px] hover:text-primary transition-colors' to={'/'}>
           Logo
         </Link>
         <nav className='my-2 flex gap-6 font-body text-xl'>
-          <NavLink className=' hover:bg-secondary hover:text-primary px-2 py-1' to={'/'}>
+          <NavLink className='hover:text-primary px-2 py-1' to={'/'}>
             Home
           </NavLink>
 
           {!isLoggedIn && (
             <>
-              <NavLink className='rounded-sm hover:bg-secondary hover:text-primary  px-2 py-1' to={'/login'}>
+              <NavLink className='hover:text-primary transition-colors  px-2 py-1' to={'/login'}>
                 Login
               </NavLink>
-              <NavLink className='rounded-sm hover:bg-secondary hover:text-primary px-2 py-1' to={'/register'}>
+              <NavLink className='hover:text-primary transition-colors px-2 py-1' to={'/register'}>
                 Sign up
               </NavLink>
             </>
           )}
           {isLoggedIn && (
             <>
-              <NavLink className='rounded-sm hover:bg-secondary hover:text-primary  px-2 py-1' to={'/shops'}>
+              <NavLink className='hover:text-primary transition-colors  px-2 py-1' to={'/shops'}>
                 Shops
               </NavLink>
-              <NavLink className='rounded-sm hover:bg-secondary hover:text-primary px-2 py-1' to={'/addshop'}>
+              <NavLink className='hover:text-primary transition-colors px-2 py-1' to={'/addshop'}>
                 Add shop
               </NavLink>
-              <button className='rounded-sm hover:bg-secondary hover:text-primary px-2 py-1' onClick={logout}>
+              <button className='hover:text-primary transition-colors px-2 py-1' onClick={logout}>
                 Logout
               </button>
             </>
