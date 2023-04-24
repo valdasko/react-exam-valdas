9b9d7e7a1a9daac6246ddb04536eda749a62a6f4
Valdas <valdaskontenis1@gmail.com>
2023-04-24 14:38:05 +0300

    shop korteles mobile atitrauktos nuo krasto

--- src/pages/Shops.jsx
+++ src/pages/Shops.jsx
@@ -45,7 +45,7 @@ function Shops() {
               <GridLoader className='' size={30} color='#CDA274' />
             </div>
           ) : (
-            <div className='grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            <div className='grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 mx-1 lg:mx-0 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
               {shopsArr.map((shopObj) => (
                 <Card item={shopObj} key={shopObj.uid} />
               ))}
